e9c712cb8ac7d30c0b89706ee3ecd188dff74c91
Zaya <heavenphrince@gmail.com>
2024-03-14 14:54:17 +0100

    authentication included

--- flask_app/api/v1/views/terminals.py
+++ flask_app/api/v1/views/terminals.py
@@ -2,7 +2,14 @@
 
 from api.v1.views import views
 from flask import jsonify
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
+
+users = [{'username': 'zaya',
+            'passwd': 'tutu'},
+        {'username': 'joseph',
+            'passwd': 'josh'}]
 
 terminals = [{'terminal_id': '12345',
                 'ptst': 'posman',
@@ -14,6 +21,25 @@ terminals = [{'terminal_id': '12345',
                 'ptst': 'posman',
                 'merchant': 'spar'}]
 
+pterminals = terminals
+
+
+@auth.verify_password
+def verify_password(username, passwd):
+    for user in users:
+        if user['username'] == username and user['passwd'] == passwd:
+            return True
+    return False
+
 @views.route('/terminals')
-def terminals():
-    return (terminals)
+def all_terminals():
+    return terminal
+
+
+@views.route('/pterminals')
+@auth.login_required
+def all_pterminals():
+    pterminals.append({'terminal_id': '97531',
+                        'ptst': 'roadrun',
+                        'merchant': 'spar'})
+    return (pterminals)
